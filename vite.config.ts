c341536ccb912ee5e3f78b4da0385b256a3e712c
Ahmed Mohamed <ahmed.cysec25@gmail.com>
2026-07-17 01:52:40 +0300

    Test PWA prompt mode

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), VitePWA({
-  registerType: "autoUpdate",
+  registerType: "prompt",
 
   includeAssets: [
   "favicon.png",
